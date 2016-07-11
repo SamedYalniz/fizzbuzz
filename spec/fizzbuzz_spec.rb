d0b1c03e0a820c9488f726c1cde824ab43b739d6
Samed Yalniz <s.yalniz@hotmail.de>
2016-07-11 08:34:57 +0200

    fizzbuzz reset, completion no.1

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
@@ -14,3 +15,33 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(1)).to eq 1
   end 
 end
+=======
+require "../fizzbuzz/fizzbuzz.rb"
+describe "fizzbuzz" do 
+	it "3/3" do
+		expect(divisible_by_three?(3)).to be true
+	end 
+	it "1/3" do 
+		expect(divisible_by_three?(1)).to be  false
+	end 
+	it "5/5" do 
+		expect(divisible_by_five?(5)).to be true
+	end 
+	it "1/5" do 
+		expect(divisible_by_five?(1)).to be false
+	end 
+	it "15/15" do 
+		expect(divisible_by_fifteen?(15)).to be true
+	end 
+	it "fizz" do 
+		expect(fizzbuzz(3)).to eq "fizz"
+	end 
+	it "buzz" do 
+		expect(fizzbuzz(5)).to eq "buzz"
+	end 
+	it "fizzbuzz" do 
+		expect(fizzbuzz(15)).to eq "fizzbuzz"
+	end 
+
+end 
+>>>>>>> 09440576eb46b69d5c43dfc0e2a24cd5d6d31dde
